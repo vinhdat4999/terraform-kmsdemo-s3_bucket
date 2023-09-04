b679b74a6c757ba006792481591af88b220fb607
DatNguyen <vinhdat4999@gmail.com>
2023-09-04 20:50:29 +0700

    first commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "bucket_tags" {
+  description = "Tags for the S3 bucket"
+  type        = map(string)
+  default = {
+    Name        = "environment"
+    Environment = "dev"
+  }
+}
